d55b28a31866e3010d27c4b7ec6debc83bca33b7
skyqnaqna <201411241@sangmyung.kr>
2019-04-13 05:42:14 +0900

    update 2493

--- Algorithm/Algorithm/BOJ_2493.cpp
+++ Algorithm/Algorithm/BOJ_2493.cpp
@@ -1,5 +1,5 @@
 /*
-백준 2493번 카드 구매하기
+백준 2493번 탑
 https://www.acmicpc.net/problem/2493
 
 각각의 탑에서 왼쪽으로 발사한 레이저가 어느 탑에서
@@ -12,7 +12,7 @@ https://www.acmicpc.net/problem/2493
 
 using namespace std;
 
-void GetReceiveTower(const int);
+void GetReceivableTower(const int);
 
 int tower[500001] = { 0 };
 
@@ -22,26 +22,41 @@ int main()
 	cin >> n;
 
 
-	for (int  i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> tower[i];
 	}
 
-	GetReceiveTower(n);
+	GetReceivableTower(n);
 
-
-	delete[] tower;
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << tower[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
 
-void GetReceiveTower(const int n)
+void GetReceivableTower(const int n)
 {
+	if (n == 1)
+	{
+		tower[n] = 0; 
+		return;
+	}
+	
 	for (int i = n-1; i > 0; --i)
 	{
 		if (tower[n] <= tower[i])
 		{
-
+			tower[n] = i;
+			GetReceivableTower(n - 1);
+			return;
 		}
+		
 	}
+	tower[n] = 0;
+	GetReceivableTower(n - 1);
+	return;
 }
